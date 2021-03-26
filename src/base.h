a4d68719a7701c08b3afec2b44da00d4fb7aaf3d
jinxes61 <853528589@qq.com>
2021-03-26 14:06:16 +0800

    Update base.h
    the def of the chess

--- src/base.h
+++ src/base.h
@@ -3,7 +3,18 @@
 
 // 与游戏有关的基本参数
 
-
+enum Chess
+{
+	EMPTY = 0,
+	BOUNDARY = 255,
+	R_JU = 1, B_JU = 101,
+	R_MA = 2, B_MA = 102,
+	R_XIANG = 3, B_XIANG = 103,
+	R_SHI = 4, B_SHI = 104,
+	R_JIANG = 5, B_JIANG = 105,
+	R_PAO = 6, B_PAO = 106,
+	R_BING = 7, B_BING = 107
+};
 
 
 
