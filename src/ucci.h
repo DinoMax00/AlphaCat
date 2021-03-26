68f1f220061b4148f14011fa9422e107adbde10e
DinoMax00 <527663226@qq.com>
2021-03-26 16:38:35 +0800

    upd ucci

--- src/ucci.h
+++ src/ucci.h
@@ -1,8 +1,29 @@
-#ifndef UCCI_H
+#ifndef UCCI_H 
 #define UCCI_H
 
-namespace ucci{
-    
-}
+#include <iostream>
+#include <string.h>
+#include <vector>
+
+class UcciEngine
+{
+private:
+    // Board board;
+    // 读入的命令字符串
+    std::string commandStr;
+    // 命令字符串分割后的结果
+    std::vector<std::string> commandVec;
+    // 读入命令
+    void getCommand();
+    // 分割字符串
+    void split();
+    // 清空字符串与命令vector
+    void clear();
+public:
+    // 引擎启动
+    void bootEngine();
+    // 正式启动
+    void run();
+};
 
 #endif
