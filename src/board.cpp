0120761c56733cef589458761523db7d46946546
fuhefei <934019256@qq.com>
2021-03-26 20:52:12 +0800

    random

--- src/board.cpp
+++ src/board.cpp
@@ -2,7 +2,7 @@
 
 #include"board.h"
 #include"base.h"
-
+#include<random>
 Board::Board() 
 {
 	buildBoardFromFen("rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR");
@@ -135,3 +135,10 @@ void Board::printBoardForDebug()
 	}
 }
 
+Move Board::randomRunMove() 
+{
+	Move x("a0a0");
+	if (mov.empty())
+		return x;
+	return mov[rand()%mov.size()];
+}
